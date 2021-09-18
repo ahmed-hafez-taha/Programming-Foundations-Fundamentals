7532401a78f7218a600dbf50159b1b4ab2a476f1
Ahmed Abdelhafez <90811701+ahmed-t-7@users.noreply.github.com>
2021-09-18 16:12:03 +0200

    Create Challenge_Guessing_Game.py

---
+++ 4. Conditional Code/Challenge_Guessing_Game.py
@@ -0,0 +1,13 @@
+#/
+#Create A program that ask the user "What's my favorite food?"
+#If the user enter the name of your favorite food, output: "Yep? So amazing!"
+#If the user dosen't enter the name of your favorite food, output: "Yuck! That's not it!"
+#/
+
+my_favorite_food = 'beans'
+favorite_food = input("What's my favorite food? ")
+
+if my_favorite_food == favorite_food:
+    print("Yep? So amazing!")
+else:
+    print("Yuck! That's not it!")
