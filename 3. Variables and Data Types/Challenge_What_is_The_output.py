ad292e45990cd541c690e930c470ac7fa08c6039
Ahmed Abdelhafez <90811701+ahmed-t-7@users.noreply.github.com>
2021-09-18 15:58:11 +0200

    Create Challenge_What_is_The_output.py

---
+++ 3. Variables and Data Types/Challenge_What_is_The_output.py
@@ -0,0 +1,15 @@
+print("Challenge 1:")
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message)  # show the message on the screen this statement will print the first message variable
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+
+result = 5 - 3  #Change the value of variable from 8 To 2
+#print("5 - 3 =", result)  #This is a comment statement willn't print anything
+
+print("Challenge complete!")  # This Will print Challenge complete
