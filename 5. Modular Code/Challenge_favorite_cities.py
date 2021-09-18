aec7ece30b39519827d762d266fe3575f3722ca3
Ahmed Abdelhafez <90811701+ahmed-t-7@users.noreply.github.com>
2021-09-18 16:28:41 +0200

    Create Challenge_favorite_cities.py

---
+++ 5. Modular Code/Challenge_favorite_cities.py
@@ -0,0 +1,12 @@
+# The Requirements of This Program
+# 1- Has a function named favorite_city
+# 2- favorite_city should have one parameter: name
+# 3- You should call favorite_city at least three times
+# 4- The output should include: "One of my favorite cities is"
+
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("Aswan")
+favorite_city("MarsaMatrouh")
+favorite_city("Hurghada")
